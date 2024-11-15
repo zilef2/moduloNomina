84c13ccafbbc5b8621fd390379bf4c5a655f0c82
Alejo Madrid <ajelof2@gmail.com>
2024-11-15 13:28:03 -0500

    to repair

--- app/Models/CentroCosto.php
+++ app/Models/CentroCosto.php
@@ -70,16 +70,19 @@ class CentroCosto extends Model
             $parametros = Parametro::find(1);
         }
         $diffMinutes = Carbon::now()->diffInMinutes($this->update_at);
-        if($diffMinutes > 60*24) {
+        if($diffMinutes >= 0) {  //todo: torepair //12mins
             $BaseDeReportes = Reporte::Where('centro_costo_id', $this->id)->get();
-        }else{
+        }
+        else{
             $diaquincena = date("d");
             $dayMedio = 15;
             $operacion = $diaquincena > $dayMedio ? '>' : '<=';
             $BaseDeReportes = Reporte::Where('centro_costo_id', $this->id)
+                ->WhereYear('fecha_ini',date("Y"))
                 ->WhereMonth('fecha_ini',date("m"))->WhereDay('fecha_ini',$operacion,$dayMedio)
                 ->get();
         }
+
         if($BaseDeReportes->count() > 0){
             $vardiurnas = 0;
             $varnocturnas = 0;
@@ -89,21 +92,23 @@ class CentroCosto extends Model
             $vardominical_nocturno = 0;
             $vardominical_extra_diurno = 0;
             $vardominical_extra_nocturno = 0;
-//            foreach ($BaseDeReportes as $index => $baseDeReporte) {
+
+            //todo: colocar $parametros->porcentaje_diurno * $sal como variable
+            foreach ($BaseDeReportes as $index => $baseDeReporte) {
                 $user = User::find($baseDeReporte->user_id);
                 if($user){
                     $sal = $user->salario / 235;
 
-                    $vardiurnas += ((double)$BaseDeReportes->sum('diurnas')) * $parametros->porcentaje_diurno * $sal;
-                    $varnocturnas += ((double)$BaseDeReportes->sum('nocturnas')) * $parametros->porcentaje_nocturno * $sal;
-                    $varextra_diurnas += ((double)$BaseDeReportes->sum('extra_diurnas')) * $parametros->porcentaje_extra_diurno * $sal;
-                    $varextra_nocturnas += ((double)$BaseDeReportes->sum('extra_nocturnas')) * $parametros->porcentaje_extra_nocturno * $sal;
-                    $vardominical_diurno += ((double)$BaseDeReportes->sum('dominical_diurno')) * $parametros->porcentaje_dominical_diurno * $sal;
-                    $vardominical_nocturno += ((double)$BaseDeReportes->sum('dominical_nocturno')) * $parametros->porcentaje_dominical_nocturno * $sal;
-                    $vardominical_extra_diurno += ((double)$BaseDeReportes->sum('dominical_extra_diurno')) * $parametros->porcentaje_dominical_extra_diurno * $sal;
-                    $vardominical_extra_nocturno += ((double)$BaseDeReportes->sum('dominical_extra_nocturno')) * $parametros->porcentaje_dominical_extra_nocturno * $sal;
+                    $vardiurnas += ((double)$baseDeReporte->sum('diurnas')) * $parametros->porcentaje_diurno * $sal;
+                    $varnocturnas += ((double)$baseDeReporte->sum('nocturnas')) * $parametros->porcentaje_nocturno * $sal;
+                    $varextra_diurnas += ((double)$baseDeReporte->sum('extra_diurnas')) * $parametros->porcentaje_extra_diurno * $sal;
+                    $varextra_nocturnas += ((double)$baseDeReporte->sum('extra_nocturnas')) * $parametros->porcentaje_extra_nocturno * $sal;
+                    $vardominical_diurno += ((double)$baseDeReporte->sum('dominical_diurno')) * $parametros->porcentaje_dominical_diurno * $sal;
+                    $vardominical_nocturno += ((double)$baseDeReporte->sum('dominical_nocturno')) * $parametros->porcentaje_dominical_nocturno * $sal;
+                    $vardominical_extra_diurno += ((double)$baseDeReporte->sum('dominical_extra_diurno')) * $parametros->porcentaje_dominical_extra_diurno * $sal;
+                    $vardominical_extra_nocturno += ((double)$baseDeReporte->sum('dominical_extra_nocturno')) * $parametros->porcentaje_dominical_extra_nocturno * $sal;
                 }
-//            }
+            }
 
             $this->mano_obra_estimada = $vardiurnas+ $varnocturnas+ $varextra_diurnas+ $varextra_nocturnas+ $vardominical_diurno+ $vardominical_nocturno+ $vardominical_extra_diurno+ $vardominical_extra_nocturno;
             $this->update();
