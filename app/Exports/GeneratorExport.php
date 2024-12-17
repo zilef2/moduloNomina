6ab3744b6972d011b5864dfe65529dd573c80d79
zilef2 <ajelof2@gmail.com>
2024-12-17 15:46:21 -0500

    excelcontroller incompleto

--- app/Exports/GeneratorExport.php
+++ app/Exports/GeneratorExport.php
@@ -12,17 +12,18 @@ class GeneratorExport implements FromGenerator
 
     public function generator(): Generator
     {
-        $todosCC = CentroCosto::select(['nombre', 'descripcion','activo'])
+        $todosCC = CentroCosto::select(['nombre', 'descripcion','activo','ValidoParaFacturar'])
             ->orderByDesc('activo')
 //            ->Where('activo', 1)
             ->get();
-        
+
         $i = 0;
         yield [
             'Numero',
             'Centro',
             'Descripcion',
             'Activo',
+            'ValidoParaFacturar',
         ];
         foreach ($todosCC as $index => $item) {
             $i++;
@@ -30,9 +31,10 @@ class GeneratorExport implements FromGenerator
                 $i,
                 $item['nombre'],
                 $item['descripcion'],
-                $item['activo'] == 1 ? 'SI' : 'NO',
+                $item['activo'] == 1 ? 'Si' : 'No',
+                $item['ValidoParaFacturar'] == 1 ? 'Si' : 'No',
             ];
         }
-        
+
     }
 }
