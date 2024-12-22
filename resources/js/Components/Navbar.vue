bcebe26a59ec59fe2ac6200e809e3f73fe5bea0f
Alejo Madrid <ajelof2@gmail.com>
2024-12-22 14:25:27 -0500

    dashboard dia mes una letra y el año completo

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -15,7 +15,7 @@ const emit = defineEmits(["open"])
 //     Inertia.visit('/users/export', { method: 'get', download: true });
 // }
 
-let version = "1.24_5d2024"
+let version = "22d2024"
 const downloadExcel = async () => {
     window.open('/users/export', '_blank')
 }
